8dc7307619654947e32846e75ad8ed2bc24b22b0
Sindhukumari Parameswaran <33718216+sindhusweety@users.noreply.github.com>
2024-04-04 13:40:13 +0530

    Add files via upload

--- sql_main.py
+++ sql_main.py
@@ -6,6 +6,23 @@ try:
     conn = sqlite3.connect('Data Engineer_ETL Assignment.db')
     cursor = conn.cursor()
 
+    # Query to get all tables in the database
+    sql_query = """
+    SELECT name FROM sqlite_master WHERE type='table'
+    """
+
+    # Execute the SQL query
+    cursor.execute(sql_query)
+
+    # Fetch the results
+    tables = cursor.fetchall()
+
+    # Print the list of tables
+    print("Tables in the database:")
+    for table in tables:
+        print(table[0])
+
+
     # SQL solution
     # Query to extract total quantities of each item bought per customer aged 18-35
     sql_query = """
@@ -19,15 +36,18 @@ try:
     HAVING quantity > 0
     """
 
-    # Execute the SQL query and fetch the results
-    sql_results = pd.read_sql_query(sql_query, conn)
+    # Execute the SQL query
+    cursor.execute(sql_query)
 
-    # Pandas solution
-    # Filter customers aged 18-35
-    customers_filtered = sql_results[sql_results['age'].between(18, 35)]
+    # Fetch the results
+    results = cursor.fetchall()
 
-    # Store the DataFrame to a CSV file
-    customers_filtered.to_csv('output.csv', index=False, sep=';')
+    # Store the results in a CSV file
+    with open('output_sql.csv', 'w') as file:
+        file.write("Customer;Age;Item;Quantity\n")
+        for row in results:
+            file.write(";".join(map(str, row)) + "\n")
+
 
 except sqlite3.Error as e:
     print("SQLite error:", e)
