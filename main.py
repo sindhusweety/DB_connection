d627665d5f6e0d9402a9c75fdba1950e6c2286c3
Sindhukumari Parameswaran <33718216+sindhusweety@users.noreply.github.com>
2024-04-04 16:01:18 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,22 +6,27 @@ try:
     conn = sqlite3.connect('Data Engineer_ETL Assignment.db')
     cursor = conn.cursor()
 
-    df_sales = pd.read_sql_query("SELECT * FROM sales", conn)
-    df_order = pd.read_sql_query("SELECT * FROM `orders`", conn)
-    df_customer = pd.read_sql_query("SELECT * FROM customers", conn)
-    df_items = pd.read_sql_query("SELECT * FROM items", conn)
+    sales = pd.read_sql_query("SELECT * FROM sales", conn)
+    orders = pd.read_sql_query("SELECT * FROM `orders`", conn)
+    customers = pd.read_sql_query("SELECT * FROM customers", conn)
+    items = pd.read_sql_query("SELECT * FROM items", conn)
 
-    df = pd.merge(df_order, df_sales, on='sales_id')
-    df = pd.merge(df, df_customer, on='customer_id')
-    df = pd.merge(df, df_items, on='item_id')
+    # Merge tables
+    merged_data = pd.merge(customers, sales, on='customer_id')
+    merged_data = pd.merge(merged_data, orders, on='sales_id')
+    merged_data = pd.merge(merged_data, items, on='item_id')
 
-    df_filtered = df[(df['age'] >= 18) & (df['age'] <= 35) & (df['quantity'] > 0)]
+    # Filter customers aged 18-35
+    customers_filtered = merged_data[merged_data['age'].between(18, 35)]
 
-    df_grouped = df_filtered.groupby(['customer_id', 'age', 'item_name'])['quantity'].sum().reset_index()
+    # Group by customer_id, age, and item_name, summing up the quantity
+    grouped_data = customers_filtered.groupby(['customer_id', 'age', 'item_name']).agg({'quantity': 'sum'}).reset_index()
 
-    df_grouped.rename(columns={'customer_id': 'Customer', 'item_name': 'Item', 'quantity': 'Quantity'}, inplace=True)
+    # Omit items with no purchase (quantity = 0)
+    grouped_data = grouped_data[grouped_data['quantity'] > 0]
 
-    df_grouped.to_csv('output.csv', index=False, sep=';')
+    # Write the DataFrame to a CSV file
+    grouped_data.to_csv('output.csv', index=False, sep=';')
 
 except sqlite3.Error as e:
     print("SQLite error:", e)
